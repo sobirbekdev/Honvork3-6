9f152afe4c4f41bd93a49ce621db050315961f96
sobirbekdev <122030584+sobirbekdev@users.noreply.github.com>
2023-02-26 16:44:42 +0500

    mexmonxona xarajatlari qo'shildi

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
 let Math =prompt("Iltimos Summangizdi kriting ");
 
 let x=500; //Borish va kelish samaliyot bileti narxi $
-let y=120 //muzey va ko'ngil ochar joylar uchon yevro
-let z=10650.34 // 1$ so'm
-let t=11650.03 //  1 yevro so'm
+let t=11650.03; //  1 yevro so'm
+let a=250; //mexmonxona to'lovi uchun
+let y=120; //muzey va ko'ngil ochar joylar uchon yevro
+let z=10650.34; // 1$ so'm
 
-let dollorKursi=(x*z);
+let dollorKursi=((x+a)*z);
 let yevroKursi =(y*t);
-let hisobot=(dollorKursi+yevroKursi)
 
+let hisobot=(dollorKursi+yevroKursi)
 let saytTitle = document.querySelector ("#title")
 
 
-if (0<Math && Math >hisobot){
-    saytTitle.textContent ="Oq yo' sizda yetarli mablag' mavjud"
-  console.log(" Oq yo' sizda yetarli mablag' mavjud ")
-}else if(0<Math && Math<hisobot){
+if (Math>=hisobot){
+    saytTitle.textContent ="Oq yo'l sizda yetarli mablag' mavjud"
+  console.log(" Oq yo'l sizda yetarli mablag' mavjud ")
+}else if(Math<hisobot){
     saytTitle.textContent=" Sizda yetarli mablag' mavjud ems biroz sabir qiling."
  console.log (" Sizda yetarli mablag' mavjud ems biroz sabir qiling. ")
 };
